5bcad23f08e00d5b35b5d51077594f9a7db0582c
Ryuma Saito <ryuma@RyumanoMacBook-Pro.local>
2017-10-31 23:40:02 +0900

    コンパス使うにはキャリブ必須か

--- Quadrino.ino
+++ Quadrino.ino
@@ -3,6 +3,7 @@
 //  -----------------------------------------------------------------------------
 
 #include <Wire.h>           // Include I2C communications
+#include <MadgwickAHRS.h>
 #include "Venus838FLP.h"    // GPS Functions
 #include "MS5611.h"         // MS5611 Functions
 #include "MPU9150.h"        // MPU9150 Functions
@@ -18,6 +19,7 @@
 
 //  Setup Variables
 //  -----------------------------------------------------------------------------
+Madgwick AHRS;
 
 String inputString = "";         // Serial Comm: a string to hold incoming data
 boolean stringComplete = false;  // Serial Comm: whether the string is complete
@@ -32,6 +34,7 @@ float latlonCenter[2] = {0.5 * (latlonCp[0][0] + latlonCp[1][0]),0.5 * (latlonCp
 void setup(){
     Serial.begin(115200);            // Start Serial Port at 57600 baud
     Wire.begin();                   // Initialize the 'Wire' class for the I2C-bus.
+    AHRS.begin(100.0f);
     inputString.reserve(200);       // SerialCommunications: reserve 200 bytes for the inputString:
     GPSModuleInit();                // Venus838FLP: Startup GPS Module
     GPSConfigureDefaults();         // Venus838FLP: Configure Default Values
@@ -115,18 +118,57 @@ void RotAroudZ(float* x,float* y,float* z,float angleRad)
   *y = buf[1];
 }
 
+void ReadIMU(void)
+{
+  float afx,afy,afz;
+  float gfx,gfy,gfz;
+  float mfx,mfy,mfz;
+  afx = convertRawAcceleration(MPU9150_readSensor(MPU9150_ACCEL_XOUT_L,MPU9150_ACCEL_XOUT_H));
+  afy = convertRawAcceleration(MPU9150_readSensor(MPU9150_ACCEL_YOUT_L,MPU9150_ACCEL_YOUT_H));
+  afz = convertRawAcceleration(MPU9150_readSensor(MPU9150_ACCEL_ZOUT_L,MPU9150_ACCEL_ZOUT_H));
+  gfx = convertRawGyro(MPU9150_readSensor(MPU9150_GYRO_XOUT_L,MPU9150_GYRO_XOUT_H));
+  gfy = convertRawGyro(MPU9150_readSensor(MPU9150_GYRO_YOUT_L,MPU9150_GYRO_YOUT_H));
+  gfz = convertRawGyro(MPU9150_readSensor(MPU9150_GYRO_ZOUT_L,MPU9150_GYRO_ZOUT_H));
+  mfx = (float)MPU9150_readSensor(MPU9150_CMPS_XOUT_L,MPU9150_CMPS_XOUT_H);
+  mfy = (float)MPU9150_readSensor(MPU9150_CMPS_YOUT_L,MPU9150_CMPS_YOUT_H);
+  mfz = (float)MPU9150_readSensor(MPU9150_CMPS_ZOUT_L,MPU9150_CMPS_ZOUT_H);
+#if 0
+  Serial.print(",Ax,");
+  Serial.print(convertRawAcceleration(MPU9150_readSensor(MPU9150_ACCEL_XOUT_L,MPU9150_ACCEL_XOUT_H)));
+  Serial.print(",Ay,");
+  Serial.print(convertRawAcceleration(MPU9150_readSensor(MPU9150_ACCEL_YOUT_L,MPU9150_ACCEL_YOUT_H)));
+  Serial.print(",Az,");
+  Serial.print(convertRawAcceleration(MPU9150_readSensor(MPU9150_ACCEL_ZOUT_L,MPU9150_ACCEL_ZOUT_H)));
+  Serial.print(",Gx,");
+  Serial.print(convertRawGyro(MPU9150_readSensor(MPU9150_GYRO_XOUT_L,MPU9150_GYRO_XOUT_H)));
+  Serial.print(",Gy,");
+  Serial.print(convertRawGyro(MPU9150_readSensor(MPU9150_GYRO_YOUT_L,MPU9150_GYRO_YOUT_H)));
+  Serial.print(",Gz,");
+  Serial.print(convertRawGyro(MPU9150_readSensor(MPU9150_GYRO_ZOUT_L,MPU9150_GYRO_ZOUT_H)));
+  Serial.print("Mx");
+  Serial.print(MPU9150_readSensor(MPU9150_CMPS_XOUT_L,MPU9150_CMPS_XOUT_H));
+  Serial.print("My");
+  Serial.print(MPU9150_readSensor(MPU9150_CMPS_YOUT_L,MPU9150_CMPS_YOUT_H));
+  Serial.print("Mz");
+  Serial.println(MPU9150_readSensor(MPU9150_CMPS_ZOUT_L,MPU9150_CMPS_ZOUT_H));
+#endif
+  AHRS.update(gfx, gfy, gfz, afx, afy, afz, mfy, mfx, mfz);
+  //AHRS.updateIMU(gfx, gfy, gfz, afx, afy, afz);
+  Serial.print(",yawAngle,");Serial.print(AHRS.getYawRadians()); //ヨー角
+}
+
 void DebugGPS(void)
 {
-  Serial.print("fix,");Serial.print(venus_ctx.location.fixmode);
-  Serial.print("lon,");Serial.print(venus_ctx.location.longitude);
-  Serial.print("lat,");Serial.print(venus_ctx.location.latitude);
-  Serial.print("height,");Serial.print(venus_ctx.location.ellipsoid_alt * 0.01);
-  Serial.print("ecef.x,");Serial.print((float)venus_ctx.location.ecef.x * 0.01);
-  Serial.print("ecef.y,");Serial.print((float)venus_ctx.location.ecef.y * 0.01);
-  Serial.print("ecef.z,");Serial.print((float)venus_ctx.location.ecef.z * 0.01);
-  Serial.print("spd.x,");Serial.print((float)venus_ctx.location.vel.x * 0.01);
-  Serial.print("spd.y,");Serial.print((float)venus_ctx.location.vel.y * 0.01);
-  Serial.print("spd.z,");Serial.print((float)venus_ctx.location.vel.z * 0.01);
+  Serial.print(",fix,");Serial.print(venus_ctx.location.fixmode);
+  Serial.print(",lon,");Serial.print(venus_ctx.location.longitude);
+  Serial.print(",lat,");Serial.print(venus_ctx.location.latitude);
+  Serial.print(",height,");Serial.print(venus_ctx.location.ellipsoid_alt * 0.01);
+  Serial.print(",ecef.x,");Serial.print((float)venus_ctx.location.ecef.x * 0.01);
+  Serial.print(",ecef.y,");Serial.print((float)venus_ctx.location.ecef.y * 0.01);
+  Serial.print(",ecef.z,");Serial.print((float)venus_ctx.location.ecef.z * 0.01);
+  Serial.print(",spd.x,");Serial.print((float)venus_ctx.location.vel.x * 0.01);
+  Serial.print(",spd.y,");Serial.print((float)venus_ctx.location.vel.y * 0.01);
+  Serial.print(",spd.z,");Serial.print((float)venus_ctx.location.vel.z * 0.01);
 
 }
 
@@ -134,19 +176,41 @@ void DebugGPS(void)
 void loop ()
 {
     float e,n,u,velmps,heading;
-    VenusRead(1000);
-
- 
+    VenusRead(1000); 
     GetPosENU(&e,&n,&u);
-    Serial.print("e,");Serial.print(e);
-    Serial.print("n,");Serial.print(n);
-    Serial.print("u,");Serial.print(u);
     GetVelAndHead(&velmps,&heading);
-    Serial.print("Vel,");Serial.print(velmps);
-    Serial.print("Head,");Serial.print(heading);
-
-
-    DebugGPS();
+ #if 0   
+    Serial.print(",e,");Serial.print(e);
+    Serial.print(",n,");Serial.print(n);
+    Serial.print(",u,");Serial.print(u);
+    Serial.print(",Vel,");Serial.print(velmps);
+    Serial.print(",Head,");Serial.print(heading);
+#endif
+    ReadIMU();
+    //DebugGPS();
     Serial.println(",");
-    delay(50);
+    delay(10);
+}
+
+
+/************************************************************************
+ * FUNCTION : IMU加速度生値->物理値変換
+ * INPUT    : IMU加速度生値
+ * OUTPUT   : 加速度(m/s^2)
+ ***********************************************************************/
+float convertRawAcceleration(int aRaw)
+{
+   float a = (aRaw * 2.0 * 9.80665) / 32768.0;
+   return a;
+}
+
+/************************************************************************
+ * FUNCTION : IMU角速度生値->物理値変換
+ * INPUT    : IMU角速度生値
+ * OUTPUT   : 角速度(deg/s)
+ ***********************************************************************/
+float convertRawGyro(int gRaw)
+{
+   float g = (gRaw * 1000.0f) / 32768.0;
+   return g;
 }
